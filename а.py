487de05ddfe7bee7b7c0a1d4271421ce9a39fd3b
npax1 <prakh55555@gmail.com>
2022-12-03 11:19:06 +0200

    add

--- а.py
+++ а.py
@@ -1,37 +1,11 @@
-// Importing
-required
-classes
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+from itertools import islice
 
-// Main
+f1 = open('Test.txt')
+f2 = open('Output.txt', 'a')
 
+bunch = 500
+lines = list(islice(f1, bunch))
+f2.writelines(lines)
 
-class
-    public
-
-    class GFG {
-
-    // Main driver method
-    public static void main(String[] args)
-    throws IOException
-    {
-    // Assigning the content of the file
-    String text
-    = "Welcome to geekforgeeks\nHappy Learning!";
-
-    // Defining the file name of the file
-    Path fileName = Path.of(
-    "/Users/mayanksolanki/Desktop/demo.docx");
-
-    // Writing into the file
-    Files.writeString(fileName, text);
-
-    // Reading the content of the file
-    String file_content = Files.readString(fileName);
-
-    // Printing the content inside the file
-    System.out.println(file_content);
-    }
-    }
+f1.close()
+f2.close()
