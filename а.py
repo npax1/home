a8bed5da09283e734368defa9f57335ae9dc1970
npax1 <prakh55555@gmail.com>
2022-12-03 11:27:12 +0200

    add

--- а.py
+++ а.py
@@ -1,37 +1,12 @@
-// Importing
-required
-classes
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-// Main
-
-
-class
-    public
-
-    class GFG {
-
-    // Main driver method
-    public static void main(String[] args)
-    throws IOException
-    {
-    // Assigning the content of the file
-    String text
-    = "Welcome to geekforgeeks\nHappy Learning!";
-
-    // Defining the file name of the file
-    Path fileName = Path.of(
-    "/Users/mayanksolanki/Desktop/demo.docx");
-
-    // Writing into the file
-    Files.writeString(fileName, text);
-
-    // Reading the content of the file
-    String file_content = Files.readString(fileName);
-
-    // Printing the content inside the file
-    System.out.println(file_content);
-    }
-    }
+f1 = open("output1.txt", "w")
+with open("file.txt", "r") as myfile:
+    data = myfile.read()
+data_1 = data[::-1]
+f1.write(data_1)
+f1.close()
+f2 = open("output2.txt", "w")
+with open("file.txt", "r") as myfile:
+    data = myfile.readlines()
+data_2 = data[::-1]
+f2.writelines(data_2)
+f2.close()
